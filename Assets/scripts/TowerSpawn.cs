fe8f6db13741cee6f961c02514b25eb92e92977c
LiamDCreator <Liamduijn@gmail.com>
2024-10-07 21:49:27 +0200

    towerspawn script

--- Assets/scripts/TowerSpawn.cs
+++ Assets/scripts/TowerSpawn.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class TowerSpawn : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public List<GameObject> towers;
+    int spawnID = -1;
+    
+    
 }
